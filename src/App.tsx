6d46d0aa27e260ea51888eed623937f4e8549300
German Gambarte <germangambarte@gmail.com>
2024-11-20 18:21:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="font-bold text-center text-4xl py-8">
+          Planificador de Gastos
+        </h1>
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // darkMode: 'class',
+  content: ['./index.html', './src/**/*.{ts,js,tsx,jsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
